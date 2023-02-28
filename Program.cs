da26d02bde8930cfd6f9a34469142631b95a49c6
harmgehog <polstia@gmail.com>
2023-02-28 13:20:14 +0300

    Final 4 step & program.cs. DONE!

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с 
+клавиатуры, либо задать на старте выполнения алгоритма. Не использовать списки.
+*/
+
+public class SomeNewClass
+{
+    static string[] arr = { "It", "It's", "It isn't", "Kotlin!", "is!", "awesome!" };
+
+    static string[] NewArray(string[] mas)
+    {
+        int x = 0;
+        for (int i = 0; i < mas.Length; i++) if (mas[i].Length <= 3) x++;
+
+        string[] mas2 = new string[x];
+
+        int ind = 0;
+        for (int i = 0; i < mas.Length; i++) if (mas[i].Length <= 3) { mas2[ind] = mas[i]; ind++; };
+
+        return mas2;
+    }
+
+    static void ShowArray(string[] array)
+    {
+        Console.Write("[\"");
+        Console.Write(String.Join("\", \"", array));
+        Console.Write("\"]");
+    }
+
+    static void Main()
+    {
+        ShowArray(arr);
+        string[] newarr = NewArray(arr);
+        Console.Write(" -> ");
+        ShowArray(newarr);
+
+    }
+}
